285ac1ed004725c20c00fc996633e05122e26db3
Nikolas Trapp <nikolastrapp@gmail.com>
2022-09-16 22:19:43 -0300

    iniciando arquivos

---
+++ backend/src/main/java/com/nikolastrapp/coffeestand/CoffeeStandApplication.java
@@ -0,0 +1,13 @@
+package com.nikolastrapp.coffeestand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeStandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeStandApplication.class, args);
+	}
+
+}
