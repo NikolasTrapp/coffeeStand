09892cbd4fce3f268b842f49d4b4e1aaa4b067ca
Nikolas Trapp <nikolastrapp@gmail.com>
2022-09-16 22:23:35 -0300

    iniciando arquivos

---
+++ backend/src/test/java/com/nikolastrapp/coffeestand/CoffeeStandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolastrapp.coffeestand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeStandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
